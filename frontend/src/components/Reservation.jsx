8c5eda6dfc0cadbc661e470cc4bc57e17cf4703b
Aizaz Soomro <aizazsoomro9@gmail.com>
2026-01-19 18:23:59 +0500

    last

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://lame-restaurant-pkrb.vercel.app/",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
