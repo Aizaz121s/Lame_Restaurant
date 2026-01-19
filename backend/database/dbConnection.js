21eace90339dc5f43f429d951479ead5cc161b22
Aizaz Soomro <aizazsoomro9@gmail.com>
2026-01-19 19:34:29 +0500

    updated

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
+export const dbConnection = async () => {
+  // If already connected, don't try to connect again
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       dbName: "RESERVATIONS",
-    })
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`);
     });
+    console.log("Connected to database!");
+  } catch (err) {
+    console.log(`Some error occurred while connecting to database: ${err}`);
+    throw err; // Throwing the error helps Vercel logs show what happened
+  }
 };
